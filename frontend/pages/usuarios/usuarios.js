3efceff9933695318502036aefeb8eb85a000aa1
EA - Heitor Pereira Da Silva <pereira_heitor@educadventista.org>
2025-11-27 09:23:50 -0300

    backend arrumado pelo marcinho

---
+++ frontend/pages/usuarios/usuarios.js
@@ -0,0 +1,20 @@
+const main = document.querySelector("main")
+
+
+async function searchKrakens(){
+  const persons = await fetch("http://localhost:3333").then(response => response.json())
+  
+  persons.map((person, index) =>{
+    main.innerHTML += `
+            <section>
+            <h2>Nome: ${person.name}</h2>
+            <p>Email: ${person.email}</p>
+            <p>Idade: ${person.age}</p>
+            <p>Apelido: ${person.nickname}</p>
+        </section>
+        ${index + 1 === persons.length ? "" : "<hr>"}
+    `
+  })
+}
+
+searchKrakens()
---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+function inti(){
+ const navUL = document.querySelector("nav ul")
+
+ const user = JSON.parse(sessionStorage.getItem("user"))
+
+ if (user){
+    navUL.innerHTML += `
+                <li>
+                    <h2>Usuário: ${user.name}</h2>
+                </li>
+                <li>
+                    <button>Sair</button>
+                </li>
+    `
+
+    return
+ }
+
+ navUL.innerHTML += `
+ <li>
+                    <a href="./pages/login/login.html">Login</a>
+                </li>
+ `
+}
+
+inti()
